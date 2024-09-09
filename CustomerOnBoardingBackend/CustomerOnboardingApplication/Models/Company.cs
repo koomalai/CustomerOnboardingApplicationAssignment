ca52db01983de61c7f4e2b6fcb7eaf83aa821f90
koomalai appandi <appandikoomalai@gmail.com>
2024-09-09 13:56:03 +0400

    feat(Changes in Entity framework): Changed in files

---
+++ CustomerOnBoardingBackend/CustomerOnboardingApplication/Models/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomerOnboardingApplication.Models;
+
+public partial class Company
+{
+    public int CompanyId { get; set; }
+
+    public string? NameOfCompany { get; set; }
+
+    public string? TypeOfEntity { get; set; }
+
+    public string? BusinessActivity { get; set; }
+
+    public string? License { get; set; }
+
+    public string? RegistrationNumber { get; set; }
+
+    public DateTime? DateOfIncorporation { get; set; }
+
+    public int? CountryId { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public virtual Country? Country { get; set; }
+
+    public virtual Customer? Customer { get; set; }
+}
